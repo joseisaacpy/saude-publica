4091c52ea303654a05ac6f3bee476930c1a64aa7
José Isaac <joseisaacnascimento@gmail.com>
2025-07-05 14:31:03 -0300

    fix: Referecia link do css nas páginas 404 e form

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,23 @@
-// vite.config.js
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   server: {
     allowedHosts: [
       "all",
       "b6dc-2804-29b8-519d-edd6-f8e6-d871-fec9-c760.ngrok-free.app",
     ],
   },
-};
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    cssCodeSplit: false, // Mantém todo CSS em um arquivo
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  css: {
+    postcss: "./postcss.config.js",
+  },
+});
