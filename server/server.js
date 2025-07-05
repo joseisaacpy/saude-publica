4ec1cdfc001c33a66f3b3dbe69452533fc1e7c33
José Isaac <joseisaacnascimento@gmail.com>
2025-07-05 12:37:35 -0300

    feat: Cria estrutura inicial do server.js, usando type module - Faz imports - Usa middlewares do express - Cria rota de rota não encontrada - Cria arquivo 404.html vazio

---
+++ server/server.js
@@ -0,0 +1,33 @@
+// Imports
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+// import adminRoutes from "./routes/adminRoutes.js";
+
+// Configurando o dotenv
+dotenv.config();
+
+// Constantes
+const app = express();
+const PORT = 3000;
+
+// Middlewares
+app.use(cors()); // Habilita o CORS
+app.use(express.json()); // Habilita o JSON
+app.use(express.urlencoded({ extended: true })); // Habilita o URL Encoded (Para enviar formularios)
+
+// Rotas
+app.get("/", (req, res) => {
+  res.send("API rodando");
+});
+
+
+// Rota não encontrada
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Rota não encontrada" });
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
